04c4b973b541ca5e989a976417a89f7deee8e83c
Csaba Fabian <csaba.fabian@t-systems.com>
2022-01-07 11:34:18 +0100

    replace str with variable values

--- Backstage/backstage-values.sh
+++ Backstage/backstage-values.sh
@@ -4,7 +4,7 @@
 #do
 #  sed -i 's/$sec/\${{ secrets.$sec }}/g' backstage-values.yaml
 #done
-sed -i 's/PG_HOST/${{ secrets.PG_HOST }}/g' Backstage/backstage-values.yaml
-sed -i 's/PG_PORT/${{ secrets.PG_PORT }}/g' Backstage/backstage-values.yaml
-sed -i 's/PG_USER/${{ secrets.CATENAX_ADMIN_USER }}/g' Backstage/backstage-values.yaml
-sed -i 's/PG_PASS/${{ secrets.CATENAX_ADMIN_PASSWORD }}/g' Backstage/backstage-values.yaml
+sed -i "s/PG_HOST/${{ secrets.PG_HOST }}/g" Backstage/backstage-values.yaml
+sed -i "s/PG_PORT/${{ secrets.PG_PORT }}/g" Backstage/backstage-values.yaml
+sed -i "s/PG_USER/${{ secrets.CATENAX_ADMIN_USER }}/g" Backstage/backstage-values.yaml
+sed -i "s/PG_PASS/${{ secrets.CATENAX_ADMIN_PASSWORD }}/g" Backstage/backstage-values.yaml
